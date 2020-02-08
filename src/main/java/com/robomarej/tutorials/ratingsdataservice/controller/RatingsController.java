5f281dce4136ceec5044e5a6dda2811c77a2cdfd
robo <robomarej@gmail.com>
2020-02-08 11:51:48 +0100

    rest api for user`s movies

--- src/main/java/com/robomarej/tutorials/ratingsdataservice/controller/RatingsController.java
+++ src/main/java/com/robomarej/tutorials/ratingsdataservice/controller/RatingsController.java
@@ -1,9 +1,12 @@
 package com.robomarej.tutorials.ratingsdataservice.controller;
 
 import com.robomarej.tutorials.ratingsdataservice.model.Rating;
+import com.robomarej.tutorials.ratingsdataservice.model.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,5 +15,15 @@ public class RatingsController {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId){
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getRatings(@PathVariable("userId") String userId){
+        UserRating userRating = new UserRating();
+        userRating.setRatings(Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        ));
+        return userRating;
     }
 }
--- src/main/java/com/robomarej/tutorials/ratingsdataservice/model/UserRating.java
+++ src/main/java/com/robomarej/tutorials/ratingsdataservice/model/UserRating.java
@@ -4,11 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Rating {
+public class UserRating {
 
-    private String movieId;
-    private int rating;
+    private List<Rating> ratings;
 }
